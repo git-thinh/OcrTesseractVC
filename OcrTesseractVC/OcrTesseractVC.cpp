403a3478f38f703a36e2851b034755e1ba009455
nguyenvanthinh <nguyenvanthinh@f88.vn>
2020-01-31 12:13:44 +0700

    a

--- OcrTesseractVC/OcrTesseractVC.cpp
+++ OcrTesseractVC/OcrTesseractVC.cpp
@@ -1,15 +1,246 @@
-// OcrTesseractVC.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 #include <tesseract/baseapi.h>
 #include <leptonica/allheaders.h>
-using namespace std;
 
+using namespace std;
 using namespace tesseract;
 
 class TessAPI : public TessBaseAPI { public: void PrintRects(int len); };
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+//#include <winsock2.h>
+//#include <stdio.h>
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
+
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+
+int tcp_client_SendText(string text, char* s_ip_address, char* s_port)
+{
+	WSADATA              wsaData;
+
+	SOCKET               SendingSocket;
+
+	// Server/receiver address
+
+	SOCKADDR_IN          ServerAddr, ThisSenderInfo;
+
+	// Server/receiver port to connect to
+
+	unsigned int         Port = 7171;
+
+	int  RetCode;
+
+	// Be careful with the array bound, provide some checking mechanism...
+
+	char sendbuf[1024] = "This is a test string from sender";
+
+	int BytesSent, nlen;
+
+
+
+	// Initialize Winsock version 2.2
+
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	printf("Client: Winsock DLL status is %s.\n", wsaData.szSystemStatus);
+
+
+
+	// Create a new socket to make a client connection.
+
+	// AF_INET = 2, The Internet Protocol version 4 (IPv4) address family, TCP protocol
+
+	SendingSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (SendingSocket == INVALID_SOCKET)
+
+	{
+
+		printf("Client: socket() failed! Error code: %ld\n", WSAGetLastError());
+
+		// Do the clean up
+
+		WSACleanup();
+
+		// Exit with error
+
+		return -1;
+
+	}
+
+	else
+
+		printf("Client: socket() is OK!\n");
+
+
+
+	// Set up a SOCKADDR_IN structure that will be used to connect
+
+	// to a listening server on port 5150. For demonstration
+
+	// purposes, let's assume our server's IP address is 127.0.0.1 or localhost
+
+
+
+	// IPv4
+
+	ServerAddr.sin_family = AF_INET;
+
+	// Port no.
+
+	ServerAddr.sin_port = htons(Port);
+
+	// The IP address
+
+	PCWSTR ip = (PCWSTR)"127.0.0.1";
+	//ServerAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	InetPton(AF_INET, ip, &ServerAddr.sin_addr.s_addr);
+
+	// Make a connection to the server with socket SendingSocket.
+
+	RetCode = connect(SendingSocket, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr));
+
+	if (RetCode != 0)
+
+	{
+
+		printf("Client: connect() failed! Error code: %ld\n", WSAGetLastError());
+
+		// Close the socket
+
+		closesocket(SendingSocket);
+
+		// Do the clean up
+
+		WSACleanup();
+
+		// Exit with error
+
+		return -1;
+
+	}
+
+	else
+
+	{
+
+		printf("Client: connect() is OK, got connected...\n");
+
+		printf("Client: Ready for sending and/or receiving data...\n");
+
+	}
+
+
+
+	// At this point you can start sending or receiving data on
+
+	// the socket SendingSocket.
+
+
+
+	// Some info on the receiver side...
+
+	getsockname(SendingSocket, (SOCKADDR*)&ServerAddr, (int*)sizeof(ServerAddr));
+
+	//printf("Client: Receiver IP(s) used: %s\n", inet_ntoa(ServerAddr.sin_addr));
+
+	printf("Client: Receiver port used: %d\n", htons(ServerAddr.sin_port));
+
+
+
+	// Sends some data to server/receiver...
+
+	BytesSent = send(SendingSocket, sendbuf, strlen(sendbuf), 0);
+
+
+
+	if (BytesSent == SOCKET_ERROR)
+
+		printf("Client: send() error %ld.\n", WSAGetLastError());
+
+	else
+
+	{
+
+		printf("Client: send() is OK - bytes sent: %ld\n", BytesSent);
+
+		// Some info on this sender side...
+
+		// Allocate the required resources
+
+		memset(&ThisSenderInfo, 0, sizeof(ThisSenderInfo));
+
+		nlen = sizeof(ThisSenderInfo);
+
+
+
+		getsockname(SendingSocket, (SOCKADDR*)&ThisSenderInfo, &nlen);
+
+		//printf("Client: Sender IP(s) used: %s\n", inet_ntoa(ThisSenderInfo.sin_addr));
+
+		printf("Client: Sender port used: %d\n", htons(ThisSenderInfo.sin_port));
+
+		printf("Client: Those bytes represent: \"%s\"\n", sendbuf);
+
+	}
+
+
+
+	if (shutdown(SendingSocket, SD_SEND) != 0)
+
+		printf("Client: Well, there is something wrong with the shutdown(). The error code : % ld\n", WSAGetLastError());
+
+	else
+
+			printf("Client: shutdown() looks OK...\n");
+
+	// When you are finished sending and receiving data on socket SendingSocket,
+
+	// you should close the socket using the closesocket API. We will
+
+	// describe socket closure later in the chapter.
+
+	if (closesocket(SendingSocket) != 0)
+
+		printf("Client: Cannot close \"SendingSocket\" socket. Error code: %ld\n", WSAGetLastError());
+
+	else
+
+		printf("Client: Closing \"SendingSocket\" socket...\n");
+
+
+
+	// When your application is finished handling the connection, call WSACleanup.
+
+	if (WSACleanup() != 0)
+
+		printf("Client: WSACleanup() failed!...\n");
+
+	else
+
+		printf("Client: WSACleanup() is OK...\n");
+
+	return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 //const char* p_file = "C:\\Git\\OCR\\OCR_Accord.Tesseract\\data-test\\text\\phototest.tif";
 const char* p_file = "C:\\ocr-images\\1.1.jpg";
@@ -91,7 +322,7 @@ int test_box_GetComponentImages_001(const char* file) {
 		exit(1);
 	}
 
-	api->SetImage(image);
+	api->SetImage(image);  
 
 	Boxa* boxes = api->GetComponentImages(tesseract::RIL_TEXTLINE, true, NULL, NULL);
 	//printf("\nFound %d textline image components.\n", boxes->n);
@@ -164,13 +395,66 @@ int test_box_BoundingBox_001(const char* file) {
 	return 0;
 }
 
+string line_get_XYWH(const char* file) {
+	Pix* image = pixRead(file);
+
+	tesseract::TessBaseAPI* api = new tesseract::TessBaseAPI();
+	//api->Init(NULL, "eng");
+	// Initialize tesseract-ocr with English, without specifying tessdata path
+	if (api->Init(NULL, "eng")) {
+		fprintf(stderr, "Could not initialize tesseract.\n");
+		exit(1);
+	}
+
+	api->SetImage(image);
+
+	Boxa* boxes = api->GetComponentImages(tesseract::RIL_TEXTLINE, true, NULL, NULL);
+	//printf("\nFound %d textline image components.\n", boxes->n);
+
+	string text = "[\n";
+	int len = boxes->n;
+	for (int i = 0; i < len; i++) {
+		BOX* box = boxaGetBox(boxes, i, L_CLONE);
+		//api->SetRectangle(box->x, box->y, box->w, box->h);
+		//int conf = api->MeanTextConf();		
+		//const char* ocrResult = api->GetUTF8Text();
+		//fprintf(stdout, "Box[%d]: x=%d, y=%d, w=%d, h=%d, confidence: %d, text: %s", i, box->x, box->y, box->w, box->h, conf, ocrResult);
+		//delete[] ocrResult;
+
+		//fprintf(stdout, "\nBox[%d]: x=%d, y=%d, w=%d, h=%d\n", i, box->x, box->y, box->w, box->h);
+
+		text += "[" + std::to_string(i) + "," + std::to_string(box->x) + "," + std::to_string(box->y) + "," + std::to_string(box->w) + "," + std::to_string(box->h) + "]";
+		if (i == len - 1)
+			text += "\n";
+		else
+			text += ",\n";
+
+		boxDestroy(&box);
+	}
+
+	text += "\n]";
+
+	std::cout << text;
+
+	// Destroy used object and release memory
+	api->End();
+	delete api;
+	pixDestroy(&image);
+
+	return text;
+}
+
 int main()
 {
 	//test_GetUTF8Text_001(p_file); // Ok
 	//test_GetUTF8Text_002(p_file); // Error
 
-	test_box_GetComponentImages_001(p_file); // Ok
+	//test_box_GetComponentImages_001(p_file); // Ok
 	//test_box_BoundingBox_001(p_file); // Ok
+
+	string text = line_get_XYWH(p_file);
+
+
 
 	return 0;
 }
